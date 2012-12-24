ead8022af85b105ec4ad384065cdc94d1d02feef
Paul Soucy <paul@dev-smart.com>
2012-12-24 12:39:00 -0500

    thread working

--- src/TSThread.m
+++ src/TSThread.m
@@ -4,4 +4,24 @@
 @implementation TSThread
 
 
+-(id) initWithTarget:(id)aTarget
+			selector:(SEL)aSelector
+			object:(id)anArgument
+{
+	_target = RETAIN(aTarget);
+	_selector = aSelector;
+	_argument= RETAIN(anArgument);
+
+	return self;
+}
+
+-(void) dealloc
+{
+	RELEASE(_target);
+	_selector = nil;
+	RELEASE(_argument);
+
+	[super dealloc];
+}
+
 @end
--- src/tinystep/TSThread.h
+++ src/tinystep/TSThread.h
@@ -2,7 +2,23 @@
 #import <tinystep/TSObject>
 
 @interface TSThread : TSObject {
-	
+
+#ifdef THREAD_SUPPORT
+	pthread_t thread_obj;
+#endif
+
+	id _target;
+	SEL _selector;
+	id _argument;
+
 }
+
++(TSThread*) currentThread;
+
+-(id) initWithTarget:(id)aTarget 
+			selector:(SEL)aSelector
+			object:(id) anArgument; 
+
+-(void)start;
 
 @end
