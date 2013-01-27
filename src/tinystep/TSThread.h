3d92ada41a524521b563f3ef49f34532a7ada96f
Paul Soucy <paul@dev-smart.com>
2013-01-27 14:55:06 -0500

    added license to all files

--- src/tinystep/TSThread.h
+++ src/tinystep/TSThread.h
@@ -1,3 +1,23 @@
+/*
+    Copyright 2013 Paul Soucy
+
+    This file is part of TinyStep.
+
+    TinyStep is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    TinyStep is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU Lesser Public License
+    along with TinyStep.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 
 #import <tinystep/TSObject.h>
 #import <tinystep/TSAutoreleasePool.h>
